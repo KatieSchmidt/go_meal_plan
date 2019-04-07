96f366a0015e6439bbb762af4165002f79570dbf
Katie Schmidt <katie_schmidt7@icloud.com>
2019-04-07 15:37:38 -0400

    check if meal already exists under said username

--- main.go
+++ main.go
@@ -7,6 +7,7 @@ import (
 	"log"
 	"net/http"
 	"time"
+	// "reflect"
 	"github.com/gorilla/mux"
 	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/mongo"
@@ -27,27 +28,36 @@ type ErrorMessage struct {
 func CreateMeal(response http.ResponseWriter, request *http.Request) {
 	request.ParseForm()
 	response.Header().Set("content-type", "application/x-www-form-urlencoded")
-	//create Meal using the form data, save to a collection,
 
 	if len(request.FormValue("username")) == 0 || len(request.FormValue("mealname")) == 0{
 		meal_error := ErrorMessage{"One of your form fields was empty"}
 		json.NewEncoder(response).Encode(meal_error)
 	} else {
+		// look for a meal that has same username and meal name
+			// if it does return an error if not, make the meal
+		collection := client.Database("go_meals").Collection("meals")
+		ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+		defer cancel()
 		var meal Meal
 		meal.Username = request.FormValue("username")
 		meal.Mealname = request.FormValue("mealname")
-		collection := client.Database("go_meals").Collection("meals")
-		//cancel will cancel ctx as soon as timeout expires
-		ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
-		defer cancel()
-		_, err := collection.InsertOne(ctx, meal)
-
-		if err != nil {
-			response_message := ErrorMessage{"ERROR: there was an error creating your meal"}
-			json.NewEncoder(response).Encode(response_message)
+		filter := bson.D{{"username", meal.Username}, {"mealname", meal.Mealname}}
+		var resulting_meal Meal
+		error_msg := collection.FindOne(ctx, filter).Decode(&resulting_meal)
+		//if the meal wasnt found, create it else send an error message
+		if error_msg != nil {
+			//create Meal using the form data, save to a collection,
+			_, err := collection.InsertOne(ctx, meal)
+			if err != nil {
+				response_message := ErrorMessage{"ERROR: there was an error creating your meal"}
+				json.NewEncoder(response).Encode(response_message)
+			} else {
+				//if there isnt an error, meal was inserted, so return the meal
+				json.NewEncoder(response).Encode(meal)
+			}
 		} else {
-			//if there isnt an error, meal was inserted, so return the meal
-			json.NewEncoder(response).Encode(meal)
+			response_message := ErrorMessage{"meal already exists"}
+			json.NewEncoder(response).Encode(response_message)
 		}
 	}
 }
