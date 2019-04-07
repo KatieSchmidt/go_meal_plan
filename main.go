1778951daa0ce7819f0d433164bac47b3606f28c
Katie Schmidt <katie_schmidt7@icloud.com>
2019-04-07 12:22:40 -0400

    add cancel and err add print statements for developmental learning

--- main.go
+++ main.go
@@ -7,6 +7,7 @@ import (
 	"log"
 	"net/http"
 	"time"
+	"reflect"
 	"github.com/gorilla/mux"
 	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/mongo"
@@ -16,27 +17,49 @@ import (
 var client *mongo.Client
 
 type Meal struct {
-	Username string  `json:"username,omitempty" bson:"username,omitempty"`
-	Mealname  string `json:"mealname,omitempty" bson:"mealname,omitempty"`
+	Username string  `json:"username" bson:"username"`
+	Mealname  string `json:"mealname" bson:"mealname"`
 }
 
 func CreateMeal(response http.ResponseWriter, request *http.Request) {
 	request.ParseForm()
 	response.Header().Set("content-type", "application/x-www-form-urlencoded")
 
-	//ctreate Meal using the form data, save to a collection,
+	//create Meal using the form data, save to a collection,
 	var meal Meal
 	meal.Username = request.FormValue("username")
 	meal.Mealname = request.FormValue("mealname")
-	collection := client.Database("meal_plan").Collection("meals")
-	ctx, _ := context.WithTimeout(context.Background(), 5*time.Second)
-	result, _ := collection.InsertOne(ctx, meal)
-	json.NewEncoder(response).Encode(result)
+	collection := client.Database("go_meals").Collection("meals")
+	//cancel will cancel ctx as soon as timeout expires
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+	fmt.Println("meal: ", meal, reflect.TypeOf(meal))
+	result, err := collection.InsertOne(ctx, meal)
+
+	if err != nil {
+		type ErrorMessage struct {
+			Error string
+		}
+		response_message := ErrorMessage{"ERROR: there was an error creating your meal"}
+		json.NewEncoder(response).Encode(response_message)
+	} else {
+		fmt.Println(result, reflect.TypeOf(result))
+		// filter := bson.D{"_id", result}
+		single_result := collection.FindOne(ctx, result)
+		fmt.Println("single_result: ", single_result, reflect.TypeOf(single_result))
+
+		var new_meal Meal
+		single_result.Decode(&new_meal)
+		fmt.Println("new_meal: ", new_meal, reflect.TypeOf(new_meal))
+		json.NewEncoder(response).Encode(new_meal)
+		}
+
+
 }
 
 func GetMeals(response http.ResponseWriter, request *http.Request) {
 	response.Header().Set("content-type", "application/json")
-	collection := client.Database("g_meals").Collection("meals")
+	collection := client.Database("go_meals").Collection("meals")
 
 	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
 
@@ -58,14 +81,10 @@ func GetMeals(response http.ResponseWriter, request *http.Request) {
 		meals = append(meals, meal)
 	}
 
-	if err := cursor.Err(); err != nil {
-		response.WriteHeader(http.StatusInternalServerError)
-		return
-	}
 	if len(meals) > 0 {
 		json.NewEncoder(response).Encode(meals)
 	} else {
-		//if there are no meals create a message Struct to marshal back
+		//if there are no meals create a message Struct to send back
 		type ErrorMessage struct {
 			Error string
 		}
