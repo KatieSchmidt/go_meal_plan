32f6ca475cc99fd6d387fc8f717340483b0fb111
Katie Schmidt <katie_schmidt7@icloud.com>
2019-04-08 14:40:37 -0400

    break it up into packages so its not just one file

---
+++ models/models.go
@@ -0,0 +1,26 @@
+package models
+
+import (
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"time"
+)
+
+type Meal struct {
+	Username string  `json:"username" bson:"username"`
+	Mealname  string `json:"mealname" bson:"mealname"`
+	TotalCalories int64 `json:"totalcalories" bson:"totalcalories"`
+	Ingredients []Ingredient `json:"ingredients" bson:"ingredients"`
+	DateAdded time.Time
+}
+
+type Ingredient struct {
+	ID primitive.ObjectID `json:"ing_id" bson:"ing_id"`
+	Ingredient string `json:"ingredient" bson:"ingredient"`
+	Calories int64 `json:"calories" bson:"calories"`
+	MeasureUnitQuantity int64 `json:"measureunitquantity" bson:"measureunitquantity"`
+	MeasureUnit string `json:"measureunit" bson:"measureunit"`
+}
+
+type ErrorMessage struct {
+	Error string
+}
