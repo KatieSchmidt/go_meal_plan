682a37fe39724744ec4cdbec663de2fa17cf734d
Katie Schmidt <katie_schmidt7@icloud.com>
2019-04-08 20:29:32 -0400

    there is a bug... what did I introduce??

--- main.go
+++ main.go
@@ -10,11 +10,11 @@ import (
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 	"github.com/KatieSchmidt/meal_plan/routes/meals"
-	"github.com/KatieSchmidt/meal_plan/routes/users"
-	"github.com/KatieSchmidt/meal_plan/routes/mealplans"
-	"github.com/KatieSchmidt/meal_plan/routes/weekplans"
-	"github.com/KatieSchmidt/meal_plan/routes/grocerylists"
-	"github.com/KatieSchmidt/meal_plan/routes/weeklists"
+	// "github.com/KatieSchmidt/meal_plan/routes/users"
+	// "github.com/KatieSchmidt/meal_plan/routes/mealplans"
+	// "github.com/KatieSchmidt/meal_plan/routes/weekplans"
+	// "github.com/KatieSchmidt/meal_plan/routes/grocerylists"
+	// "github.com/KatieSchmidt/meal_plan/routes/weeklists"
 )
 
 var client *mongo.Client
@@ -30,42 +30,42 @@ func main() {
 	router := mux.NewRouter()
 
 	//user routes
-	router.HandleFunc("/users/login", users.LoginUser).Methods("POST")
-	router.HandleFunc("/users/register", users.RegisterUser).Methods("GET")
+	// router.HandleFunc("/users/login", users.LoginUser).Methods("POST")
+	// router.HandleFunc("/users/register", users.RegisterUser).Methods("GET")
 	//meals routes
-	router.HandleFunc("/meal", meals.CreateMeal).Methods("POST")
+	router.HandleFunc("/meals", meals.CreateMeal).Methods("POST")
 	router.HandleFunc("/meals", meals.GetMeals).Methods("GET")
 	router.HandleFunc("/meals/{id}", meals.GetMealById).Methods("GET")
 	router.HandleFunc("/meals/{id}/ingredients", meals.AddIngredientToMeal).Methods("PUT")
 	router.HandleFunc("/meals/{id}", meals.DeleteMealById).Methods("DELETE")
 	router.HandleFunc("/meals/{id}/remove/{ingredient_id}", meals.DeleteIngredientFromMeal).Methods("PUT")
-	//mealplan routes
-	router.HandleFunc("/mealplans", mealplans.CreateMealplan).Methods("POST")
-	router.HandleFunc("/mealplans", mealplans.GetMealplans).Methods("GET")
-	router.HandleFunc("/mealplans/{mealplan_id}", mealplans.GetMealplanById).Methods("GET")
-	router.HandleFunc("/mealplans/{mealplan_id}/{meal_id}", mealplans.AddMealToMealplan).Methods("PUT")
-	router.HandleFunc("/mealplans/{mealplan_id}", mealplans.DeleteMealplan).Methods("DELETE")
-	router.HandleFunc("/mealplans/{mealplan_id}/remove/{meal_id}", mealplans.DeleteMealFromMealplan).Methods("PUT")
-	//weekplan routes
-	router.HandleFunc("/weekplans", weekplans.CreateWeekplan).Methods("POST")
-	router.HandleFunc("/weekplans", weekplans.GetWeekplans).Methods("GET")
-	router.HandleFunc("/weekplans/{weekplan_id}", weekplans.GetWeekplanById).Methods("GET")
-	router.HandleFunc("/weekplans/{weekplan_id}/{mealplan_id}", weekplans.AddMealplanToWeekplan).Methods("PUT")
-	router.HandleFunc("/weekplans/{weekplan_id}", weekplans.DeleteWeekplan).Methods("DELETE")
-	router.HandleFunc("/weekplans/{weekplan_id}/remove/{mealplan_id}", weekplans.DeleteMealplanFromWeekplan).Methods("PUT")
-	//grocery list routes
-	router.HandleFunc("/grocerylists", grocerylists.CreateGrocerylist).Methods("POST")
-	router.HandleFunc("/grocerylists", grocerylists.GetGrocerylists).Methods("GET")
-	router.HandleFunc("/grocerylists/{grocerylist_id}", grocerylists.GetGrocerylistById).Methods("GET")
-	router.HandleFunc("/grocerylists/{grocerylist_id}/{mealplan_id}", grocerylists.AddMealplanToGrocerylist).Methods("PUT")
-	router.HandleFunc("/grocerylists/{grocerylist_id}", grocerylists.DeleteGrocerylist).Methods("DELETE")
-	router.HandleFunc("/grocerylists/{grocerylist_id}/remove/{mealplan_id}", grocerylists.DeleteMealplanFromGrocerylist).Methods("PUT")
-	//weekly grocery list routes
-	router.HandleFunc("/weeklists", weeklists.CreateWeeklist).Methods("POST")
-	router.HandleFunc("/weeklists", weeklists.GetWeeklists).Methods("GET")
-	router.HandleFunc("/weeklists/{weeklist_id}", weeklists.GetWeeklistById).Methods("GET")
-	router.HandleFunc("/weeklists/{weeklist_id}/{weekplan_id}", weeklists.AddWeekplanToWeeklist).Methods("PUT")
-	router.HandleFunc("/weeklists/{weeklist_id}", weeklists.DeleteWeeklist).Methods("DELETE")
-	router.HandleFunc("/weeklists/{weeklist_id}/remove/{weekplan_id}", weeklists.DeleteWeekplanFromWeeklist).Methods("PUT")
+	// //mealplan routes
+	// router.HandleFunc("/mealplans", mealplans.CreateMealplan).Methods("POST")
+	// router.HandleFunc("/mealplans", mealplans.GetMealplans).Methods("GET")
+	// router.HandleFunc("/mealplans/{mealplan_id}", mealplans.GetMealplanById).Methods("GET")
+	// router.HandleFunc("/mealplans/{mealplan_id}/{meal_id}", mealplans.AddMealToMealplan).Methods("PUT")
+	// router.HandleFunc("/mealplans/{mealplan_id}", mealplans.DeleteMealplan).Methods("DELETE")
+	// router.HandleFunc("/mealplans/{mealplan_id}/remove/{meal_id}", mealplans.DeleteMealFromMealplan).Methods("PUT")
+	// //weekplan routes
+	// router.HandleFunc("/weekplans", weekplans.CreateWeekplan).Methods("POST")
+	// router.HandleFunc("/weekplans", weekplans.GetWeekplans).Methods("GET")
+	// router.HandleFunc("/weekplans/{weekplan_id}", weekplans.GetWeekplanById).Methods("GET")
+	// router.HandleFunc("/weekplans/{weekplan_id}/{mealplan_id}", weekplans.AddMealplanToWeekplan).Methods("PUT")
+	// router.HandleFunc("/weekplans/{weekplan_id}", weekplans.DeleteWeekplan).Methods("DELETE")
+	// router.HandleFunc("/weekplans/{weekplan_id}/remove/{mealplan_id}", weekplans.DeleteMealplanFromWeekplan).Methods("PUT")
+	// //grocery list routes
+	// router.HandleFunc("/grocerylists", grocerylists.CreateGrocerylist).Methods("POST")
+	// router.HandleFunc("/grocerylists", grocerylists.GetGrocerylists).Methods("GET")
+	// router.HandleFunc("/grocerylists/{grocerylist_id}", grocerylists.GetGrocerylistById).Methods("GET")
+	// router.HandleFunc("/grocerylists/{grocerylist_id}/{mealplan_id}", grocerylists.AddMealplanToGrocerylist).Methods("PUT")
+	// router.HandleFunc("/grocerylists/{grocerylist_id}", grocerylists.DeleteGrocerylist).Methods("DELETE")
+	// router.HandleFunc("/grocerylists/{grocerylist_id}/remove/{mealplan_id}", grocerylists.DeleteMealplanFromGrocerylist).Methods("PUT")
+	// //weekly grocery list routes
+	// router.HandleFunc("/weeklists", weeklists.CreateWeeklist).Methods("POST")
+	// router.HandleFunc("/weeklists", weeklists.GetWeeklists).Methods("GET")
+	// router.HandleFunc("/weeklists/{weeklist_id}", weeklists.GetWeeklistById).Methods("GET")
+	// router.HandleFunc("/weeklists/{weeklist_id}/{weekplan_id}", weeklists.AddWeekplanToWeeklist).Methods("PUT")
+	// router.HandleFunc("/weeklists/{weeklist_id}", weeklists.DeleteWeeklist).Methods("DELETE")
+	// router.HandleFunc("/weeklists/{weeklist_id}/remove/{weekplan_id}", weeklists.DeleteWeekplanFromWeeklist).Methods("PUT")
 	log.Fatal(http.ListenAndServe(":5000", router))
 }
--- models/models.go
+++ models/models.go
@@ -6,7 +6,7 @@ import (
 )
 
 type Meal struct {
-	Username string  `json:"username" bson:"username"`
+	Username string `json:"username" bson:"username"`
 	Mealname  string `json:"mealname" bson:"mealname"`
 	TotalCalories int64 `json:"totalcalories" bson:"totalcalories"`
 	Ingredients []Ingredient `json:"ingredients" bson:"ingredients"`
@@ -23,4 +23,40 @@ type Ingredient struct {
 
 type ErrorMessage struct {
 	Error string
+}
+
+type User struct {
+	Username string `json:"username" bson:"username"`
+	Email string `json:"email" bson:"email"`
+	Password string `json:"password" bson:"password"`
+	Token string `json:"token" bson:"token"`
+}
+
+type Mealplan struct {
+	UserId string `json:"userid" bson:"userid"`
+	Planname string `json:"planname" bson:"planname"`
+	TotalCalories int64 `json:"totalcalories" bson:"totalcalories"`
+	Meals []Meal `json:"meals" bson:"meals"`
+}
+
+type Weekplan struct {
+	UserId string `json:"user" bson:"user"`
+	Planname string `json:"planname" bson:"planname"`
+	TotalCalories int64 `json:"totalcalories" bson:"totalcalories"`
+	Mealplans []Mealplan `json:"mealplans" bson:"mealplans"`
+}
+
+type GroceryList struct {
+	Mealplan primitive.ObjectID `json:"mealplan" bson:"mealplan"`
+	Groceries map[int]GroceryItem `json:"groceries" bson:"groceries"`
+}
+
+type WeekList struct {
+	Weekplan primitive.ObjectID `json:"weekplan" bson:"weekplan"`
+	Groceries map[int]GroceryItem `json:"groceries" bson:"groceries"`
+}
+
+type GroceryItem struct {
+	Ingredient string `json:"ingredient" bson:"ingredient"`
+	MeasureUnit string `json:"measureunit" bson:"measureunit"`
 }
